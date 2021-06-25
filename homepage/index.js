828639b8be3569bfa10ad2c6c64bcedf5549f607
moayadalhaj <moayad.alhaj21@gmail.com>
2021-06-25 21:39:41 +0300

    add animation for the sections

--- homepage/index.js
+++ homepage/index.js
@@ -3,8 +3,180 @@
 window.addEventListener('scroll', () => {
   let content = document.querySelector('.textBox');
   let contentPosition = content.getBoundingClientRect().top;
-  let secreenPosition = window.innerHeight;
+  let secreenPosition = window.innerHeight / 1.5;
   if (contentPosition < secreenPosition) {
     content.classList.add('active');
+  } else {
+    content.classList.remove('active');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.imgBox1');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 2;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active1');
+  } else {
+    content.classList.remove('active1');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.headingText');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active2');
+  } else {
+    content.classList.remove('active2');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.pText');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active3');
+  } else {
+    content.classList.remove('active3');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.service1');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active4');
+  } else {
+    content.classList.remove('active4');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.service2');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 0.9;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active5');
+  } else {
+    content.classList.remove('active5');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.service3');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active6');
+  } else {
+    content.classList.remove('active6');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.textBox2');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active7');
+  } else {
+    content.classList.remove('active7');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.imgBox2');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active8');
+  } else {
+    content.classList.remove('active8');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.headingText1');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active9');
+  } else {
+    content.classList.remove('active9');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.pText1');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active10');
+  } else {
+    content.classList.remove('active10');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.imgBox3');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active11');
+  } else {
+    content.classList.remove('active11');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.headingText2');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active12');
+  } else {
+    content.classList.remove('active12');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.contentBox1');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active13');
+  } else {
+    content.classList.remove('active13');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.contentBox2');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active14');
+  } else {
+    content.classList.remove('active14');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.headingText3');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active15');
+  } else {
+    content.classList.remove('active15');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.pText2');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active16');
+  } else {
+    content.classList.remove('active16');
+  }
+});
+window.addEventListener('scroll', () => {
+  let content = document.querySelector('.form');
+  let contentPosition = content.getBoundingClientRect().top;
+  let secreenPosition = window.innerHeight / 1.5;
+  if (contentPosition < secreenPosition) {
+    content.classList.add('active17');
+  } else {
+    content.classList.remove('active17');
   }
 });
