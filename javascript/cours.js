b35a5a8e4a34f6f6fac665c9f0c2120789d1f821
moayadalhaj <moayad.alhaj21@gmail.com>
2021-06-28 09:19:16 +0300

    add description for the courses

--- javascript/cours.js
+++ javascript/cours.js
@@ -2,32 +2,32 @@
 
 let showCourses = document.getElementById('coursesBox');
 let designCourses = [
-  ['Creative thinking using Adobe photoshop', 'Tawfeeq Alnajar', '$20'],
-  ['Branding using Adobe illustrator', 'Waleed Afifi', '$25'],
-  ['Design layout using Adobe Indesign', 'Adham Mhaydat', '$15'],
-  ['Social Media Design', 'Ahmad Abdulraheem', '$15'],
-  ['WEB AND DIGITAL DESIGN', 'Moayad Alhaj', '$15'],
-  ['INTERIOR AND PRODUCT DESIGN', 'Afnan shihab', '$15'],
-  ['INDUSTRIAL DESIGN', 'Ghofran Aljabr', '$15'],
-  ['VISUAL DESIGN', 'Moayad Alhaj', '$20']];
+  ['Creative thinking using Adobe photoshop', 'Tawfeeq Alnajar', '$20', 'Using a conversational tone and an approach that emphasizes the creative process, this book offers inspiration and suggestions on using Photoshop, illustrated with the author\'s own extensive collection of original work.'],
+  ['Branding using Adobe illustrator', 'Waleed Afifi', '$25', 'Illustrator is the industry-standard vector graphics application that lets you create logos, icons, sketches, typography and complex illustrations for print, web, interactive, video and mobile devices.'],
+  ['Design layout using Adobe Indesign', 'Adham Mhaydat', '$15', 'Adobe InDesign is the industry-leading layout and page design software. Create beautiful graphic designs with typography from the world՚s top foundries and imagery from Adobe Stock. Quickly share content and feedback in PDF. '],
+  ['Social Media Design', 'Ahmad Abdulraheem', '$15', 'This course is packed full of exciting, valuable lessons that teach you exactly how to create beautiful graphics that will help turn your online presence into something truly remarkable.'],
+  ['WEB AND DIGITAL DESIGN', 'Moayad Alhaj', '$15', 'The course will equip you with all the skills necessary to understand information and interface design, and the ability to plan and design a multi-page website project, for both desktop and mobile.'],
+  ['INTERIOR AND PRODUCT DESIGN', 'Afnan shihab', '$15', 'Designing contemporary furniture for volume production or creating one-off bespoke designs - technology, innovation, and style hold the key to success.'],
+  ['INDUSTRIAL DESIGN', 'Ghofran Aljabr', '$15', 'ndustrial designers apply design principles to products intended for mass production. Product design must account for the complex nature of a production pipeline, and the design process usually involves multiple components.'],
+  ['VISUAL DESIGN', 'Moayad Alhaj', '$20', 'Gain fluency in the fundamental tools of visual communication design — typography, color, and layout. GA’s visual design advisory board curates the innovative teaching approaches.']];
 let programmingCourse = [
-  ['Introduction to Game Development', 'Tawfeeq Alnajar', '$20'],
-  ['Web Programming with JavaScript', 'Waleed Afifi', '$25'],
-  ['Introduction to Computer Science', 'Adham Mhaydat', '$15'],
-  ['Understanding Technology', 'Ahmad Abdulraheem', '$15'],
-  ['Introduction to Programming with Scratch', 'Moayad Alhaj', '$15'],
-  ['Computer Science for Business', 'Afnan shihab', '$15'],
-  ['Business Analytics Program', 'Ghofran Aljabr', '$15'],
-  ['Using Python for Research', 'Moayad Alhaj', '$20']];
+  ['Introduction to Game Development', 'Tawfeeq Alnajar', '$20', 'The course explores principles of 2D and 3D graphics, animation, sound, and collision detection using frameworks like Unity and LÖVE 2D, as well as languages like Lua and C#.'],
+  ['Web Programming with JavaScript', 'Waleed Afifi', '$25', 'Topics include database design, scalability, security, and user experience. Through hands-on projects, you\'ll learn to write and use APIs, create interactive UIs, and leverage cloud services like GitHub and Heroku.'],
+  ['Introduction to Computer Science', 'Adham Mhaydat', '$15', 'It aims to provide students with an understanding of the role computation can play in solving problems.'],
+  ['Understanding Technology', 'Ahmad Abdulraheem', '$15', 'Designed for students who work with technology every day but don’t necessarily understand how it all works underneath the hood or how to solve problems when something goes wrong,'],
+  ['Introduction to Programming with Scratch', 'Moayad Alhaj', '$15', 'Course that is a fundamental course for all students who are new to computer programming. This course introduces a student to the world of Scratch, which is a block-based programming language that was created at MIT.'],
+  ['Computer Science for Business', 'Afnan shihab', '$15', 'course with a focus on innovation and cutting-edge technology. To get the best from the course you need to be interested in exploring how organisations work and how they can be improved and in developing clear logical ideas'],
+  ['Business Analytics Program', 'Ghofran Aljabr', '$15', 'Beginning with basic descriptive statistics and progressing to regression analysis, you’ll implement analytical techniques in Excel and apply fundamental quantitative methods to real business problems'],
+  ['Using Python for Research', 'Moayad Alhaj', '$20', 'This course bridges the gap between introductory and advanced courses in Python. Go deep enough to apply Python skills to research projects.']];
 let marketingCourse = [
-  ['Digital marketing', 'Tawfeeq Alnajar', '$20'],
-  ['Social media marketing', 'Waleed Afifi', '$25'],
-  ['Mega Digital Marketing', 'Adham Mhaydat', '$15'],
-  ['Digital Marketing Secrets', 'Ahmad Abdulraheem', '$15'],
-  ['Facebook Marketing', 'Moayad Alhaj', '$15'],
-  ['Digital Marketing Strategy 2021', 'Afnan shihab', '$15'],
-  ['Digital Marketing', 'Ghofran Aljabr', '$15'],
-  ['Zero Budget Marketing System', 'Moayad Alhaj', '$20']];
+  ['Digital marketing', 'Tawfeeq Alnajar', '$20', 'This course introduces students to the science of web analytics while casting a keen eye toward the artful use of numbers found in the digital space. '],
+  ['Social media marketing', 'Waleed Afifi', '$25', 'Develop an engaging and effective social media strategy for your business Build an inbound social media strategy that delights your customers and grows your bottom line Leverage.'],
+  ['Mega Digital Marketing', 'Adham Mhaydat', '$15', 'Find your target audience, easily convince them to become your customer and buy your products. Build a effective website for marketing and sale from scratch (no coding!).'],
+  ['Digital Marketing Secrets', 'Ahmad Abdulraheem', '$15', 'This course covers all of Digital Marketing\'s major topics with a focus on Inbound Marketing and Growth Hacking techniques.'],
+  ['Facebook Marketing', 'Moayad Alhaj', '$15', 'This course includes everything you need to know to improve and optimize your paid and organic Facebook efforts. This course is meant for marketers looking for actionable and impactful ways to use this popular social network.'],
+  ['Digital Marketing Strategy 2021', 'Afnan shihab', '$15', 'Digital marketing is an innovative way to attract a large audience to your online project. By learn varity marketing techniques that you can use, such as social media marketing, content marketing, email marketing, search engine optimization.'],
+  ['Search Engine Optimization', 'Ghofran Aljabr', '$15', 'Learn to optimize website content for the best possible search engine ranking. Also, the theory behind Google search and other search engine algorithms.'],
+  ['Zero Budget Marketing System', 'Moayad Alhaj', '$20', 'This course shows how to leverage free online resources to drive amazing web traffic and grow your business. Structured method for building a foundation, and then connecting all your sites.']];
 let renderCourses = document.getElementById('renderCourses');
 
 function render(courses) {
@@ -53,7 +53,7 @@ function render(courses) {
     pElement2.textContent = `price: ${courses[i][2]}`;
     divElement2.appendChild(pElement2);
     let pElement3 = document.createElement('p');
-    pElement3.textContent = 'Using a conversational tone and an approach that emphasizes the creative process, this book offers inspiration and suggestions on using Photoshop, illustrated with the author\'s own extensive collection of original work.';
+    pElement3.textContent = courses[i][3];
     divElement2.appendChild(pElement3);
     let button = document.createElement('button');
     button.id = `add${i}`;
