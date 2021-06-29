5536f7b7a6d1c8fdee2195d908c975c6474bbb46
moayadalhaj <moayad.alhaj21@gmail.com>
2021-06-29 19:19:28 +0300

    restyle course and footer for allpages

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,15 +1,4 @@
 'use strict'
-let showUserName = document.getElementById('showUserName');
-
-function showName() {
-  let userName = JSON.parse(localStorage.getItem('userData'));
-  if (userName) {
-    for (let i = 0; i < userName.length; i++) {
-      showUserName.textContent = userName[i].name;
-    }
-  }
-}
-showName();
 
 let showDataCart = document.getElementById('showDataCart');
 let tableCart = document.getElementById('tableCart');
--- javascript/reg.js
+++ javascript/reg.js
@@ -1,40 +1,40 @@
 'use strict'
-let regForm =document.getElementById('regForm');
+let regForm = document.getElementById('regForm');
 
-function Registraion(name,password,isInstructor,major){
-  this.name=name;
-  this.password=password;
-  this.isInstructor=isInstructor;
-  this.major=major;
+function Registraion(name, password, isInstructor, major) {
+  this.name = name;
+  this.password = password;
+  this.isInstructor = isInstructor;
+  this.major = major;
   Registraion.allData.push(this);
 }
-Registraion.allData=[];
+Registraion.allData = [];
 
-function getUserData(e){
-e.preventDefault();
-let name = e.target.fullName.value;
-let password= e.target.password.value;
-let isInstructor=e.target.checkbox.checked;
-let major=e.target.major.value;
+function getUserData(e) {
+  e.preventDefault();
+  let name = e.target.fullName.value;
+  let password = e.target.password.value;
+  let isInstructor = e.target.checkbox.checked;
+  let major = e.target.major.value;
 
- new Registraion(name,password,isInstructor,major);
- saveToLocal();
- window.location.assign("/index.html");
+  new Registraion(name, password, isInstructor, major);
+  saveToLocal();
+  window.location.assign("/index.html");
 }
-regForm.addEventListener('submit',getUserData);
+regForm.addEventListener('submit', getUserData);
 
-function saveToLocal(){
-  Swal.fire('Incorrect Name Or Password')
-  
-  localStorage.setItem('userData',JSON.stringify(Registraion.allData));
+function saveToLocal() {
+  Swal.fire('Thank You For Signing Up')
+
+  localStorage.setItem('userData', JSON.stringify(Registraion.allData));
 }
 function saveData() {
   let data = JSON.parse(localStorage.getItem('userData'));
-  if(data) {
-     
+  if (data) {
 
-    Registraion.allData=data;
-    }
+
+    Registraion.allData = data;
+  }
 }
 
 saveData();
